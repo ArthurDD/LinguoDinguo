d1bfa628300b0c165121ba391979e46ce59de856
ArthurDD <arth.driant@hotmail.fr>
2021-08-08 15:44:40 +0200

    Added authentication app

--- LinguoDinguo/settings.py
+++ LinguoDinguo/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Authentication
+AUTH_USER_MODEL = 'auth.User'
